05bb94cccb6fd59cd6a273461b7cbdbbda118446
kcalderon87 <kcalderon87@gmail.com>
2016-12-09 13:18:38 -0500

    liri

--- liri.js
+++ liri.js
@@ -1,10 +1,30 @@
 var keys = require('./keys.js');
 var fs = require('fs');
-var request = require ("request");
 var twitter = require('twitter');
 var spotify = require('spotify');
+var request = require ("request");
 
 //----argvs-----
 var action = process.argv[2];
 var value = process.argv[3];
 
+//----random and switch cases-----
+function random(){
+	switch (action){
+		case 'my-tweets':
+			tweets();
+		break;
+
+		case 'spotify-this-song':
+			spotifySong();
+		break;
+
+		case 'movie-this':
+			movieThis();
+		break;
+
+		case 'do-what-it-says':
+			doWhat();
+		break;
+	}
+}
