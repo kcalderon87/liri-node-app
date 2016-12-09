262270dece44ef32fe94128321d5e8f59b3a68da
kcalderon87 <kcalderon87@gmail.com>
2016-12-09 15:02:52 -0500

    tweets finished movies spotify

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//----variables----
 var keys = require('./keys.js');
 var fs = require('fs');
 
@@ -9,7 +10,7 @@ var request = require ("request");
 var action = process.argv[2];
 var value = process.argv[3];
 
-//random function & switch cases
+//-----random  and switch cases----
 function random(){
 	switch (action){
 		case 'my-tweets':
@@ -25,7 +26,7 @@ function random(){
 		break;
 
 		case 'do-what-it-says':
-			doWhat();
+			doWhatItSays();
 		break;
 	}
 }
@@ -39,12 +40,52 @@ function myTweets(){
   		if (!error) {
   			fs.appendFile('log.txt', "!!Last 20 Tweets!!" + '\n');
     	  	for (var i = 0; i < 20; i++) {
+    	  		console.log('__Tweet: ' +tweets[i].text);
   			}		
   		}  else {
   			console.log('error detail: ' + error);
   		}    	
   		
 	});
+}
+
+//----search for a movie----
+function movieThis() {
+	var value = process.argv[3] || "Mr. Nobody";
+	var queryUrl = 'http://www.omdbapi.com/?&y=&plot=short&r=json&tomatoes=true&t=' + value;
+	request(queryUrl, function(err, response, body) {
+	if ( err ) {
+        console.log('error detail: ' + err);
+        return;
+    } else {
+		body = JSON.parse(body);
+		console.log("Title: " + body.Title);
+		console.log("Year: " + body.Year);
+		console.log("IMDB Rating: " + body.imdbRating);
+		console.log("Country: " + body.Country);
+		console.log("Language: " + body.Language);
+		console.log("Plot: " + body.Plot);
+		console.log("Actors :" + body.Actors);
+		console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+		console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+	}
+});
+}
+
+//----spotify----
+function spotifySong() {
+var value = process.argv[3] || "the sign";
+spotify.search({ type: 'track', query: value }, function(err, data) {
+    if ( err ) {
+        console.log('error detail: ' + err);
+        return;
+    } else {
+    	console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+        console.log('Song_Name: ' + data.tracks.items[0].name);
+        console.log('Preview_url: ' + data.tracks.items[0].preview_url);
+        console.log('Album: ' + data.tracks.items[0].album.name);
+    }
+ });
 }
 
 
