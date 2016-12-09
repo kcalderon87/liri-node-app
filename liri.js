0a8c636525033f74de68883ab9b3264eb4396184
kcalderon87 <kcalderon87@gmail.com>
2016-12-09 13:48:10 -0500

    20 tweets

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 var keys = require('./keys.js');
 var fs = require('fs');
+
 var twitter = require('twitter');
 var spotify = require('spotify');
 var request = require ("request");
@@ -8,11 +9,11 @@ var request = require ("request");
 var action = process.argv[2];
 var value = process.argv[3];
 
-//----random and switch cases-----
+//random function & switch cases
 function random(){
 	switch (action){
 		case 'my-tweets':
-			tweets();
+			myTweets();
 		break;
 
 		case 'spotify-this-song':
@@ -28,3 +29,93 @@ function random(){
 		break;
 	}
 }
+
+
+//----20 tweets-----
+function myTweets(){
+	var client = new twitter(keys.twitterKeys);
+	var params = {screen_name: 'kimsos'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+  		if (!error) {
+  			fs.appendFile('log.txt', "!!Last 20 Tweets!!" + '\n');
+    	  	for (var i = 0; i < 20; i++) {
+  			}		
+  		}  else {
+  			console.log('error detail: ' + error);
+  		}    	
+  		
+	});
+}
+
+
+//function to search omdbapi for a movie
+function movieThis() {
+	var value = process.argv[3] || "Mr. Nobody";
+	var queryUrl = 'http://www.omdbapi.com/?&y=&plot=short&r=json&tomatoes=true&t=' + value;
+	request(queryUrl, function(err, response, body) {
+	if ( err ) {
+        console.log('error detail: ' + err);
+        return;
+    } else {
+		body = JSON.parse(body);
+		fs.appendFile('log.txt', "_______MOVIE DETAILS_____________________" + '\n');
+		console.log("Title: " + body.Title);
+		fs.appendFile('log.txt', 'Title: ' + body.Title + '\n');
+		console.log("Year: " + body.Year);
+		fs.appendFile('log.txt', 'Year: ' + body.Year + '\n');
+		console.log("IMDB Rating: " + body.imdbRating);
+		fs.appendFile('log.txt', 'IMDB Rating: ' + body.imdbRating + '\n');
+		console.log("Country: " + body.Country);
+		fs.appendFile('log.txt', 'Country: ' + body.Country + '\n');
+		console.log("Language: " + body.Language);
+		fs.appendFile('log.txt', 'Language: ' + body.Language + '\n');
+		console.log("Plot: " + body.Plot);
+		fs.appendFile('log.txt', 'Plot: ' + body.Plot + '\n');
+		console.log("Actors :" + body.Actors);
+		fs.appendFile('log.txt', 'Actors: ' + body.Actors + '\n');
+		console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+		console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+		fs.appendFile('log.txt', 'Rotten Tomatoes URL: ' + body.tomatoURL + '\n');
+	}
+});
+}//end function movieThis
+
+
+//function to search Spotify with a song name
+function spotifySong() {
+var value = process.argv[3] || "what's my age again";
+spotify.search({ type: 'track', query: value }, function(err, data) {
+    if ( err ) {
+        console.log('error detail: ' + err);
+        return;
+    } else {
+    	fs.appendFile('log.txt', "______SONG DETAILS____________________" + '\n');
+    	console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+    	fs.appendFile('log.txt', 'Artist: ' + data.tracks.items[0].artists[0].name + '\n');
+        console.log('Song_Name: ' + data.tracks.items[0].name);
+        fs.appendFile('log.txt', 'Song Name: ' + data.tracks.items[0].name + '\n');
+        console.log('Preview_url: ' + data.tracks.items[0].preview_url);
+        fs.appendFile('log.txt', 'Preview URL: ' + data.tracks.items[0].preview_url + '\n');
+        console.log('Album: ' + data.tracks.items[0].album.name);
+        fs.appendFile('log.txt', 'Album: ' + data.tracks.items[0].album.name + '\n');
+    }
+ });
+}//end function spotifySong
+
+
+//function for doWhat
+function doWhat() {
+	fs.readFile('./random.txt', "utf8", function(err, data){
+		if (err) {
+			console.log('error detail: ' +err);
+		}
+		fs.appendFile('log.txt', "_______________DO WHAT???________________" + '\n');
+		splitData = data.split(',');
+		action = splitData[0];
+		value = splitData[1];		
+		random();
+	});
+} // end function doWhat
+
+random();
+
